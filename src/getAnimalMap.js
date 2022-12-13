323619dc0cdeeaca7f2df2100b8f61c3ca17aa92
diegocamomila <diego.camomila@gmail.com>
2022-12-13 14:27:59 -0300

    FEAT: Acresentará requisito 7, src/getAnimalMap.js

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,42 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+const { species } = data;
+// Solução utilizando reduce encontrada no repositório de Gabriel Rodrigues.
+
+function report() {
+  return species.reduce((accumulator, animal) => {
+    const local = animal.location;
+    if (!accumulator[local]) {
+      accumulator[local] = [];
+    }
+    accumulator[local].push(animal.name);
+    return accumulator;
+  }, {});
+}
+function namesReport(options) {
+  const { sorted, sex } = options;
+  return species.reduce((accumulator, animal) => {
+    if (!accumulator[animal.location]) {
+      accumulator[animal.location] = [];
+    }
+    let list = [];
+    if (sex) {
+      list = animal.residents.filter((res) => res.sex === sex).map((res) => res.name);
+    } else {
+      list = animal.residents.map((res) => res.name);
+    }
+    if (sorted) {
+      list.sort();
+    }
+    accumulator[animal.location].push({ [animal.name]: list });
+    return accumulator;
+  }, {});
+}
+function getAnimalMap(options = {}) {
+  if (!options.includeNames) {
+    return report();
+  }
+  return namesReport(options);
 }
 
 module.exports = getAnimalMap;
